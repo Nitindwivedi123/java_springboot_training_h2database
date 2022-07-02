ede534c67b9539227d6594a05d0141167311c7fc
Nitin bunker <60029906+Nitindwivedi123@users.noreply.github.com>
2022-07-03 00:05:10 +0530

    Add files via upload

---
+++ training/src/main/java/com/consultadd/controller/EployeeController.java
@@ -0,0 +1,36 @@
+package com.consultadd.controller;
+
+import com.consultadd.model.Employee;
+import com.consultadd.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EployeeController {
+    @Autowired
+    EmployeeService employeeservice;
+
+    @GetMapping("/employee")
+    public List<Employee> getemployee(){
+        return employeeservice.employeedetails();
+    }
+
+    @PostMapping("/addemployee")
+    public String addemployee(@RequestBody Employee employee){
+        return employeeservice.addemployee(employee);
+    }
+
+    @DeleteMapping("/deleteemployee")
+    public String deleteemployee(@RequestBody Employee employee){
+        return employeeservice.deleteemployee(employee);
+    }
+
+    @PutMapping("/updateemployee")
+    public String updateemployee(@RequestBody Employee employee){
+        return employeeservice.updateeemployee(employee);
+    }
+
+
+}
---
+++ training/src/main/java/com/consultadd/service/EmployeeService.java
@@ -0,0 +1,48 @@
+package com.consultadd.service;
+
+import com.consultadd.model.Employee;
+import com.consultadd.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeerepository;
+
+    public List<Employee> employeedetails(){
+        List<Employee> employeedetails= employeerepository.findAll();
+        return employeedetails;
+    }
+
+    public String addemployee(Employee employee){
+        if (employeerepository.existsById(employee.getId())){
+            return "Employee already exist.";
+        }else{
+            employeerepository.save(employee);
+            return "Employee added sucessfully";
+        }
+    }
+
+    public String deleteemployee(Employee employee){
+        if (employeerepository.existsById(employee.getId())){
+            employeerepository.delete(employee);
+            return "Employee deleted successfully.";
+        }else{
+            return "Employee does not exist.";
+        }
+    }
+
+    public String updateeemployee(Employee employee){
+        if (employeerepository.existsById(employee.getId())){
+            employeerepository.save(employee);
+            return "Employee details updated successfully.";
+        }else{
+            return "Employee does not exist.";
+        }
+    }
+
+}
